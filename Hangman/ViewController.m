236a89f671687e0d9bf66f364ee8c135959732bd
Leontien90 <leontienboere@hotmail.com>
2014-12-10 01:21:43 +0100

    Cleaned view controller. Prepared input validation.

--- Hangman/ViewController.m
+++ Hangman/ViewController.m
@@ -14,113 +14,118 @@
 @end
 
 @implementation ViewController
+
 GameController *game;
+NSUserDefaults *userDefaults;
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////
+// CONSTRUCTOR
+//////////////////////////////////////////////////////////////////////////////////////////////
 - (void)viewDidLoad;
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
     // Init user defaults
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    userDefaults = [NSUserDefaults standardUserDefaults];
     if ([userDefaults integerForKey:@"standardWordLength"] == 0)
     {
         [self setDefaultValues];
     }
     
-    // Init default guessesLeft label text
-    self.turnsLeftLabel.text = [NSString stringWithFormat:@"%0.0ld", (long)[userDefaults integerForKey:@"standardAmountOfGuesses"]];
-    
     // Hide textfield by default
     self.letterEntryField.hidden = YES;
-
-    // Display keyboard
-    self.letterEntryField.autocorrectionType = UITextAutocorrectionTypeNo;
-    [self.letterEntryField becomeFirstResponder];
     
+    // Handle keyboard
+    self.letterEntryField.autocorrectionType = UITextAutocorrectionTypeNo;  // Disable autocorrect
+    [self.letterEntryField becomeFirstResponder];                           // Display keyboard
+    
+    // Start new game
+    [self startNewGame];
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// HANDLE MEMORY WARNING
+//////////////////////////////////////////////////////////////////////////////////////////////
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// SET STANDARD USER DEFAULTS
+//////////////////////////////////////////////////////////////////////////////////////////////
+- (void)setDefaultValues
+{
+    [userDefaults setInteger: 4 forKey:@"standardWordLength"];
+    [userDefaults setInteger: 8 forKey:@"standardAmountOfGuesses"];
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// ON NEW GAME BUTTON
+//////////////////////////////////////////////////////////////////////////////////////////////
+- (IBAction)onButtonNewGame:(id)sender
+{
+    // Start new game
+    [self startNewGame];
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// START A NEW GAME
+//////////////////////////////////////////////////////////////////////////////////////////////
+- (void)startNewGame
+{
     // Init game controller
     game = [[GameController alloc] init];
     
     // Start new game
     [game newGame];
     
-    // Load wordlist
-    [game loadWordList];
-    
-    // Init current word count
-    [game getCurrentWordCount];
-    
-    // init current word array
-    [game getCurrentWordString];
-    
-    // Init word to guess (underscores) label text
-    self.wordToGuessLabel.text = [game getCurrentWordString];
-    
-    // Init amount of words label text
-    NSString *amountOfWords = [NSString stringWithFormat:@"%d", [game getCurrentWordCount]];
-    self.amountOfWords.text = amountOfWords;
-    
-    self.guessedLetters.text = @"none"; 
-    
+    // Set labels
+    self.turnsLeftLabel.text    = [NSString stringWithFormat:@"%0.0ld", (long)[userDefaults integerForKey:@"standardAmountOfGuesses"]];
+    self.guessedLetters.text    = @"none";
+    self.amountOfWords.text     = [NSString stringWithFormat:@"%d", [game getCurrentWordCount]];
+    self.wordToGuessLabel.text  = [game getCurrentWordString];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-    
-}
-
+//////////////////////////////////////////////////////////////////////////////////////////////
+// ON TEXT INPUT
+// TODO: - win scenario
+//       - lose scenario
+//       - continue scenario
+//       - info label
+//////////////////////////////////////////////////////////////////////////////////////////////
 - (IBAction)changeTextClick:(id)sender
 {
-    // Update guessedLetter label text
-    NSString *guessedLetterLowerCase = self.letterEntryField.text;
-    NSString *currentGuessedLetter = [guessedLetterLowerCase uppercaseString];
-    
-    [game guessLetter:currentGuessedLetter];
-    NSString *guessedLetters = [[game getGuessedLetterArray] componentsJoinedByString:@" "];
+    // Get letter input (uppercase)
+    NSString *currentGuessedLetter = [self.letterEntryField.text uppercaseString];
 
+    // Validate input against character set
     if ([currentGuessedLetter rangeOfCharacterFromSet:[NSCharacterSet uppercaseLetterCharacterSet]].location != NSNotFound)
     {
-        if ([currentGuessedLetter rangeOfString:guessedLetters].location != NSNotFound)
+        // Validate input does not already exist
+        if ([[[game getGuessedLetterArray] componentsJoinedByString:@""] rangeOfString:currentGuessedLetter].location == NSNotFound)
         {
-            // Update guessedLetters label text
-            self.guessedLetters.text = guessedLetters;
+            // Run algorithme
+            [game guessLetter:currentGuessedLetter];
+            
+            // Set labels
+            if ([game getGuessesLeft] > 0)
+            {
+                self.turnsLeftLabel.text = [NSString stringWithFormat:@"%d", [game getGuessesLeft]];
+            }
+            else
+            {
+                self.turnsLeftLabel.text = @"You lose :(";
+            }
+            self.guessedLetters.text     = [[game getGuessedLetterArray] componentsJoinedByString:@" "];
+            self.amountOfWords.text      = [NSString stringWithFormat:@"%d", [game getCurrentWordCount]];
+            self.wordToGuessLabel.text   = [game getCurrentWordString];
         }
     }
     
-    // reset letterEntryField after character input
+    // Reset letterEntryField
     self.letterEntryField.text = @"";
-    
-    // update label with currenWordArray
-    self.wordToGuessLabel.text = [game getCurrentWordString];
-    
-    // Update guessesLeft label text
-    if ([game getGuessesLeft] > 0)
-    {
-        self.turnsLeftLabel.text = [NSString stringWithFormat:@"%d", [game getGuessesLeft]];
-    }
-    else
-    {
-       self.turnsLeftLabel.text  = [NSString stringWithFormat:@"You lose :("];
-    }
-    
-    // Update amount of words label text
-    NSString *amountOfWords = [NSString stringWithFormat:@"%d", [game getCurrentWordCount]];
-    self.amountOfWords.text = amountOfWords;
-}
-
-- (void)setDefaultValues
-{
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-    [userDefaults setInteger: 4 forKey:@"standardWordLength"];
-    [userDefaults setInteger: 8 forKey:@"standardAmountOfGuesses"];
-}
-
-- (IBAction)newGame:(id)sender
-{
-    [self viewDidLoad];
 }
 
 @end
